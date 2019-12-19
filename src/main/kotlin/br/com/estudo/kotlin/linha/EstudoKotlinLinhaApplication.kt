aa62821e13b08b2a864dba0811bf682793986e07
jjpera <jjpera@gmail.com>
2019-12-19 17:29:49 -0300

    Add files via upload

---
+++ src/main/kotlin/br/com/estudo/kotlin/linha/EstudoKotlinLinhaApplication.kt
@@ -0,0 +1,11 @@
+package br.com.estudo.kotlin.linha
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EstudoKotlinLinhaApplication
+
+fun main(args: Array<String>) {
+	runApplication<EstudoKotlinLinhaApplication>(*args)
+}
